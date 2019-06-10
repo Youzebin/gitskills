b282c902512944ddc303474fd38f88cf6c5f7366
youzebin <1519372367@qq.com>
2019-06-11 06:52:21 +0900

    fourth commit

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
 
 
 print("test")
-
+value = True 
+while value:
+    for i in range(1,100):
+        if i == 99 :
+            value = False
+    
+print (i)
 for value in range(1,11):
     print(value)
