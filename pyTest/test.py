9a9314e941e3c581ed98d28a2f294b593015eb09
youzebin <1519372367@qq.com>
2019-09-16 12:17:16 +0800

    2019.9.16.2

---
+++ pyTest/test.py
@@ -0,0 +1,10 @@
+print("test")
+value = True 
+while value:
+    for i in range(1,100):
+        if i == 99 :
+            value = False
+    
+print (i)
+for value in range(1,11):
+    print(value)
